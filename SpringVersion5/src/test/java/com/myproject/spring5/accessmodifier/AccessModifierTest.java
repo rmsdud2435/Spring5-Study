0c74248a92ecb9348ec46813ae89dfdea5d66180
gykim <gykim@gmail.com>
2019-06-14 13:46:01 +0900

    3

---
+++ SpringVersion5/src/test/java/com/myproject/spring5/accessmodifier/AccessModifierTest.java
@@ -0,0 +1,13 @@
+package com.myproject.spring5.accessmodifier;
+
+public class AccessModifierTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PublicClass publicClass = new PublicClass();
+		publicClass.publicString = "change!";
+		publicClass.
+		
+	}
+
+}
